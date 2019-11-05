399c2f8be7799874968195430cbf8d6349380588
Ben Lugavere <b.lugavere@gmail.com>
2019-11-05 08:57:19 -0800

    demo

---
+++ examples/demo/src/app.ts
@@ -0,0 +1,84 @@
+
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+import mongoose, { Model, Document, Mongoose, Schema } from 'mongoose';
+import m2s from '../../../lib';
+import swagger from './swagger.json';
+
+const app = express();
+
+const Paw = new Schema({
+  numToes: {
+    type: Number,
+    required: true,
+  }
+});
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  hasTail: {
+    type: Boolean,
+  },
+  paws: {
+    type: [Paw],
+    required: true,
+  }
+});
+
+const Cat = mongoose.model('Cat', schema);
+
+const swagger2 = {
+  definitions: {
+    Cat: m2s(Cat),
+  },
+  host: '',
+  info: {
+    title: 'My Express App',
+    version: '1.0.0'
+  },
+  paths: {
+    '/cats': {
+      get: {
+        responses: {
+          200: {
+            description: 'All Cats',
+            schema: {
+              items: {
+                $ref: '#/definitions/Cat'
+              },
+              type: 'array'
+            }
+          }
+        },
+        produces: [
+          'application/json'
+        ],
+        tags: [
+          'Cats'
+        ],
+        parameters: []
+      }
+    }
+  },
+  securityDefinitions: {},
+  swagger: '2.0',
+  tags: []
+};
+
+// fs.writeFileSync(path.join(__dirname, './swagger.json'), JSON.stringify(swagger, null, 2));
+
+app.get('/cats', (req, res) => res.send([]));
+
+app.get('/swagger', (req, res) => res.send(swagger));
+
+app.use('/', express.static(path.join(__dirname, '../docs')));
+
+app.listen('3000', () => console.log('Listening on Port 3000'));
